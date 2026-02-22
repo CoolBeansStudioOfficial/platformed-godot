308b20f4b96a6c1ce5a482d4631d064b0e784978
CoolBeansStudioOfficial <134745254+CoolBeansStudioOfficial@users.noreply.github.com>
2026-02-21 18:51:08 -0600

    added sound effects

--- Scripts/Tiles/Checkpoint.cs
+++ Scripts/Tiles/Checkpoint.cs
@@ -3,8 +3,16 @@ using System;
 
 public partial class Checkpoint : Tile
 {
+    [Export] AudioStream checkpointSound;
+
     public override void OnBodyEntered(Node2D body)
     {
-        if (body is PlayerMovement player) LevelManager.Instance.spawnPoint = Position;
+        if (body is PlayerMovement player)
+        {
+            if (LevelManager.Instance.spawnPoint != Position) AudioManager.Instance.PlayStream(checkpointSound);
+
+            LevelManager.Instance.spawnPoint = Position;
+        }
+
     }
 }
--- Scripts/Tiles/End.cs
+++ Scripts/Tiles/End.cs
@@ -3,10 +3,13 @@ using System;
 
 public partial class End : Tile
 {
+    [Export] AudioStream victorySound;
+
     public override void OnBodyEntered(Node2D body)
     {
         if (body is PlayerMovement player)
         {
+            AudioManager.Instance.PlayStream(victorySound);
             UIManager.Instance.mainMenu.Visible = true;
             LevelManager.Instance.DestroyLevel();
         }
